1def8d8b78f8ababb5fb479c4110a9250cca4746
Sara Trochanowski <strochanowski@unc.edu>
2022-02-27 23:01:35 -0500

    updates to ex04

--- exercises/ex04_turtle.py
+++ exercises/ex04_turtle.py
@@ -8,6 +8,7 @@ colormode(255)
 # I added colormode(255) to enable RGB mode, which allows me to make my own colors by adding certain amounts
 # of red, green, and blue!
 
+
 def draw_sky(sky: Turtle, x: float, y: float) -> None:
     """Paints the sky."""
     sky.pencolor(20, 106, 154)
@@ -19,11 +20,12 @@ def draw_sky(sky: Turtle, x: float, y: float) -> None:
     sky.begin_fill()
     i: int = 0
     while (i < 4):
-    # This while loop draws a square with side lengths of 720.
+        # This while loop draws a square with side lengths of 720.
         sky.forward(720)
         sky.left(90)
         i = i + 1
     sky.end_fill()
+
 
 def draw_snowy_ground(snow: Turtle, x: float, y: float) -> None:
     """Paints the snowy ground."""
@@ -36,14 +38,15 @@ def draw_snowy_ground(snow: Turtle, x: float, y: float) -> None:
     snow.begin_fill()
     i: int = 0
     while (i < 2):
-    # This while loop draws a rectangle with side lengths of 720 and 175. Since all four side lengths are not equal,
-    # I could not use the same method with the index i as I did with the square.
+        # This while loop draws a rectangle with side lengths of 720 and 175. Since all four side lengths are not equal,
+        # I could not use the same method with the index i as I did with the square.
         snow.forward(720)
         snow.left(90)
         snow.forward(175)
         snow.left(90)
         i = i + 1
     snow.end_fill()
+
 
 def draw_snowflakes(snowflakes: Turtle, x: float, y: float) -> None:
     """Fills the sky with beautiful snowflakes."""
@@ -61,6 +64,7 @@ def draw_snowflakes(snowflakes: Turtle, x: float, y: float) -> None:
         # I chose 25 degrees because I liked how it made the snowflakes look unique and uneven.
         i = i + 1
 
+
 def draw_snowman(snowman: Turtle, x: float, y: float, radius: float) -> None:
     """Draws the snowballs that help to build two cute snowpeople! A fourth parameter (radius) was added to manipulate the size of each snowball."""
     snowman.pencolor(128, 128, 128)
@@ -74,6 +78,7 @@ def draw_snowman(snowman: Turtle, x: float, y: float, radius: float) -> None:
     # I added a fourth parameter for radius so that I could manipulate the size of each snowball later on within my main function.
     snowman.end_fill()
 
+
 def draw_heart(heart: Turtle, x: float, y: float) -> None:
     """Draws a heart to show the snowpeoples' love for eachother!"""
     heart.pencolor(214, 132, 217)
@@ -85,6 +90,7 @@ def draw_heart(heart: Turtle, x: float, y: float) -> None:
     heart.right(30)
     # I had to rotate my turtle to the right by 30 degrees prior to actually drawing the heart because it was 
     # looking oddly tilted to the left before.
+
     def heart_shape() -> None:
         """Tells the turtle the specific directions for how to paint a heart shape."""
         # I tried something fun by experimenting with how to make a heart!
@@ -97,6 +103,7 @@ def draw_heart(heart: Turtle, x: float, y: float) -> None:
     heart.begin_fill()
     heart_shape()
     heart.end_fill()
+
 
 def draw_eye(eye: Turtle, x: float, y: float) -> None:
     """Adds eyes to both snowmen!"""
@@ -111,6 +118,7 @@ def draw_eye(eye: Turtle, x: float, y: float) -> None:
     eye.end_fill()
     eye.hideturtle()
     # I added the hideturtle at the end of the draw_eye function because this is the last function called by the main function.
+
 
 def main() -> None:
     """Commands the little turtle friends to start painting!"""
@@ -135,6 +143,7 @@ def main() -> None:
     draw_eye(turtles, 35, 90)
     draw_eye(turtles, 45, 90)
     done()
+
 
 if __name__ == "__main__":
     main()
