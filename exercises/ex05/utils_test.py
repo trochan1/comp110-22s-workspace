6e55b486f6ca49da84c8b61335449f98d39ba85e
Sara Trochanowski <strochanowski@unc.edu>
2022-03-02 21:26:47 -0500

    updates to ex05 again

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -4,24 +4,29 @@ __author__ = "730489697"
 
 
 from exercises.ex05.utils import only_evens, sub, concat
+# Imports the functions from utils.py so that it can test each.
 
 
 def test_only_evens_empty() -> None:
+    """Tests if the only_evens function works when the input list is empty."""
     x: list[int] = []
     assert only_evens(x) == []
 
 
 def test_only_evens_single_item() -> None:
+    """Tests if the only_evens function works when the input list has one element."""
     x: list[int] = [2]
     assert only_evens(x) == [2]
 
 
 def test_only_evens_many_items() -> None:
+    """Tests if the only_evens function works when the input list has multiple elements."""
     x: list[int] = [1, 2, 3]
     assert only_evens(x) == [2]
 
 
 def test_sub_empty() -> None:
+    """Tests if the sub function works when the input list is empty."""
     x: list[int] = []
     y: int = 1
     z: int = 3
@@ -29,6 +34,7 @@ def test_sub_empty() -> None:
 
 
 def test_sub_single_item() -> None:
+    """Tests if the sub function works when the input list has one element."""
     x: list[int] = [2]
     y: int = 1
     z: int = 3
@@ -36,6 +42,7 @@ def test_sub_single_item() -> None:
 
 
 def test_sub_many_items() -> None:
+    """Tests if the sub function works when the input list has multiple elements."""
     x: list[int] = [1, 2, 3, 4]
     y: int = 1
     z: int = 3
@@ -43,18 +50,21 @@ def test_sub_many_items() -> None:
 
 
 def test_concat_empty() -> None:
+    """Tests if the concat function works when the input list is empty."""
     x: list[int] = []
     y: list[int] = []
     assert concat(x, y) == []
 
 
 def test_concat_single_item() -> None:
+    """Tests if the concat function works when the input list has one element."""
     x: list[int] = [1]
     y: list[int] = [2]
     assert concat(x, y) == [1, 2]
 
 
 def test_concat_many_items() -> None:
+    """Tests if the concat function works when the input list has multiple elements."""
     x: list[int] = [1, 2, 3]
     y: list[int] = [4, 5, 6]
     assert concat(x, y) == [1, 2, 3, 4, 5, 6]
